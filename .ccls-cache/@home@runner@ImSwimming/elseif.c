1ef66b5397a44793e2c4c36230c1cc050aea4199
jaerim05 <128573640+jaerim05@users.noreply.github.com>
2023-05-09 01:04:32 +0000

    mission 3
    두 수를 입력 받아 큰 수 출력하기

--- .ccls-cache/@home@runner@ImSwimming/elseif.c
+++ .ccls-cache/@home@runner@ImSwimming/elseif.c
@@ -1,19 +1,19 @@
 
-#include <stdio.h>
-main(){
-  int a;
-  printf("1번 사과, 2번 딸기, 3번 수박, 4번 배");
-  scanf("%d", &a);
+// #include <stdio.h>
+// main(){
+//   int a;
+//   printf("1번 사과, 2번 딸기, 3번 수박, 4번 배");
+//   scanf("%d", &a);
 
-  if(a==1)
-    printf("사과를 좋아하는군요!");
-  else if (a==2)
-    printf("딸기를 좋아하는군요!");
-  else if(a==3)
-    printf("수박을 좋아하는군요!");
-  else if (a==4)
-    printf("배를 좋아하는군요!");
-  else
-    printf("당신은 아마도 과일을 좋아하지 않는 것 같습니다.");
-  printf("좋아하는 과일 조사 끝");
-}
+//   if(a==1)
+//     printf("사과를 좋아하는군요!");
+//   else if (a==2)
+//     printf("딸기를 좋아하는군요!");
+//   else if(a==3)
+//     printf("수박을 좋아하는군요!");
+//   else if (a==4)
+//     printf("배를 좋아하는군요!");
+//   else
+//     printf("당신은 아마도 과일을 좋아하지 않는 것 같습니다.");
+//   printf("좋아하는 과일 조사 끝");
+// }
---
+++ misson2.c
@@ -0,0 +1,19 @@
+// #include <stdio.h>
+// main(){
+//   int a;
+//   printf("학점 입력: ");
+// scanf("%d", &a);
+
+// if(a>=90)
+//   printf("A");
+//   else if(80<=a<90)
+//   printf("B");
+// else if(70<=a<80)
+//   printf("C");
+// else if(60<=a<70)
+//   printf("D");
+// else if(50<=a<60)
+//   printf("E");
+// else if(a<50)
+//   printf("F");
+// }
